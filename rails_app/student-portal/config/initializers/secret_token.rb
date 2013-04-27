f7422c1ce30dad48be89eb2c766817b72db905af
Anoop Maniankara <maniankara@vijayan-Latitude-E6520.(none)>
2013-04-27 14:13:22 +0300

    first draft

---
+++ rails_app/student-portal/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudentPortal::Application.config.secret_token = '677c3f8418d9c1f2730314307ecaeae42044900b7e6b9188ce764b39f5e56e90a6ddb8b907e71d5112221687bebca2bd8c1b10717f1c83fa255d0ec45f2c2962'
